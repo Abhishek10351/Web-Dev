5c4abe7a2fae3cba001cac353b960d6935151e09
Abhishek Kashyap <abhik5567@gmail.com>
2023-07-27 11:39:16 +0530

    add js basics

---
+++ 11. Javascript/basicsCheatsheet.js
@@ -0,0 +1,29 @@
+// Numbers:
+1;
+-99;
+0.345345;
+
+//Making variables with let:
+let numberOfFriends = 1;
+
+//Incrementing:
+numberOfFriends += 3; //numberOfFriends is now 4
+var a = 5;
+// Variables with const
+const minimumAge = 21; //CANNOT REASSIGN!
+
+//Booleans - true or false values
+true;
+false;
+let isHappy = true;
+
+//Naming Conventions
+// Use upper camel-cased names:
+let numberOfChickens = 6; //GOOD
+console.log(numberOfChickens);
+console.error("OH NO AN ERROR!");
+let b = 5;
+let c = "lmao";
+c.log("lmao");
+// NOT THE JS WAY:
+// let number_of_chickens = 6;
