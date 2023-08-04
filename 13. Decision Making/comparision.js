0760345bf7480b7fe16a13515657dc58d677abb2
Abhishek Kashyap <abhik5567@gmail.com>
2023-08-04 23:00:44 +0530

    add logical statements

---
+++ 13. Decision Making/comparision.js
@@ -0,0 +1,15 @@
+
+1 == 1 // true
+1 == 2 // false
+1 == '1' // true
+1 === '1' // false
+1 != 2 // true
+
+// ==   equal value
+// ===  equal value and equal type
+// !==  not equal value or not equal type
+
+24 > 18 // true
+24 < 18 // false
+24 >= 18 // true
+ // use === if you want to compare the type as well 
---
+++ 13. Decision Making/js script/app.js
@@ -0,0 +1,18 @@
+let age = parseInt(prompt("Enter your age"));
+
+if (age <= 10) {
+    alert("Go HOME, kid.")
+}
+else if (age < 18) {
+    alert("You are not eligible.");
+}
+else if (age > 18) {
+    alert("You are eligible for driving license")
+}
+else {
+    alert("Enter proper age")
+}
+
+function randint(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
