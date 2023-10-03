0cc1252e6fbbd1f6248382e6a61627bd23cb19c1
Abhishek Kashyap <abhik5567@gmail.com>
2023-10-03 21:16:40 +0530

    format array cheatsheet

--- 14. Arrays/array_cheatsheet.js
+++ 14. Arrays/array_cheatsheet.js
@@ -25,17 +25,14 @@ colors.length; //3
 colors.splice(1, 0, "red-orange"); // add red-orange in 1st index
 console.log(colors.includes("red"));
 console.log(colors);
-typeof(colors)
-console.log(typeof(colors))
-
+typeof colors;
+console.log(typeof colors);
 
 // === in array doesn't compare the values but the reference of the arrays
 
 // if we make a shallow copy then it will return false
-nums = [1,2]
-numsCopy = nums
-[ 1, 2 ]
-numscont = nums.copyWithin()
-[ 1, 2 ]
-numscont
+nums = [1, 2];
+numsCopy = nums[(1, 2)];
+numscont = nums.copyWithin()[(1, 2)];
+numscont;
 // here nums===numsCopy returns true whereas numscont===nums returns false because the second one is a shallow copy
