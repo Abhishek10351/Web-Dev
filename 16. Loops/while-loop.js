ace91ec2bce39d3215641ee9261baa2ff5531a2e
Abhishek Kashyap <abhik5567@gmail.com>
2023-10-07 23:50:16 +0530

    add while loop

---
+++ 16. Loops/while-loop.js
@@ -0,0 +1,19 @@
+// while loops
+let i = 1;
+while (i <= 10) {
+    console.log(i);
+    i++;
+}
+
+console.log()
+j = 5;
+while (true) {
+    if (j == 10) {
+      
+        break; // stops loop
+    }
+    console.log(j)
+    j++;
+}
+
+// game to find a random number. it shows high or low. q to quit
