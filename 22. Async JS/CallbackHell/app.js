a1e390348c801b05852bcd6ce09cc63a9c34ae58
Abhishek Kashyap <abhik5567@gmail.com>
2024-02-06 20:16:01 +0530

    add callback hell

---
+++ 22. Async JS/CallbackHell/app.js
@@ -0,0 +1,62 @@
+// ===============
+// -CALLBACK HELL -> when you have a lot of nested callbacks, it's hard to read and maintain
+// - the code. It's also hard to debug. 
+// - It's also called the "pyramid of doom" or "the callback pyramid"
+// - It's a common problem with async code.
+// - It's a problem with code readability and maintainability.
+// - It's a problem with error handling.
+
+// > This is a simple example of callback hell:
+
+
+// YIKES!!!!!!!!!!!
+// setTimeout(() => {
+//     document.body.style.backgroundColor = 'red';
+//     setTimeout(() => {
+//         document.body.style.backgroundColor = 'orange';
+//         setTimeout(() => {
+//             document.body.style.backgroundColor = 'yellow';
+//             setTimeout(() => {
+//                 document.body.style.backgroundColor = 'green';
+//                 setTimeout(() => {
+//                     document.body.style.backgroundColor = 'blue';
+//                 }, 1000)
+//             }, 1000)
+//         }, 1000)
+//     }, 1000)
+// }, 1000)
+
+
+const delayedColorChange = (newColor, delay, doNext) => {
+    setTimeout(() => {
+        document.body.style.backgroundColor = newColor;
+        doNext && doNext();
+    }, delay)
+}
+
+// STILL A LOT OF NESTING!!!
+delayedColorChange('red', 1000, () => {
+    delayedColorChange('orange', 1000, () => {
+        delayedColorChange('yellow', 1000, () => {
+            delayedColorChange('green', 1000, () => {
+                delayedColorChange('blue', 1000, () => {
+
+                })
+            })
+        })
+    })
+});
+
+
+// searchMoviesAPI('amadeus', () => {
+//     saveToMyDB(movies, () => {
+//         //if it works, run this:
+//     }, () => {
+//         //if it doesn't work, run this:
+//     })
+// }, () => {
+//     //if API is down, or request failed
+// })
+
+
+// > It is fixed with Promises and Async/Await.
