89b3f92ce232870635efe66cda525fdd56c6257a
Abhishek Kashyap <abhik5567@gmail.com>
2024-03-04 21:15:09 +0530

    add npm installer help file

---
+++ 27. The NPM Universe/exporting/installing.js
@@ -0,0 +1,11 @@
+// we can install the package by running the following command in the terminal:
+// npm install <package-name>
+
+// --save flag is used to save the package as a dependency in the package.json file
+// npm install <package-name> --save
+
+// to install a package globally, we can use the -g flag
+// npm install <package-name> -g
+
+// after installing it globally we can't directly use it in our code , we have to link it to our project
+// npm link <package-name>
