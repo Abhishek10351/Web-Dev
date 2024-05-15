4146cfd1e1be8fec42513ae22ee0b89b40ecdd37
Abhishek Kashyap <abhik5567@gmail.com>
2024-05-16 00:21:19 +0530

    modify toggler code

--- 34 React State/react state/src/Toggler.jsx
+++ 34 React State/react state/src/Toggler.jsx
@@ -1,11 +1,19 @@
 import { useState } from "react";
 import "./Toggler.css";
-export default function Toggler(){
-    const [isHappy, setIsHappy] = useState(true)
-    const update =()=> setIsHappy(!isHappy);
+export default function Toggler() {
+    const [isHappy, setIsHappy] = useState(true);
+    const [count, setCount] = useState(0);
+    const update = () => {
+        setIsHappy(!isHappy);
+        setCount(count+1);
+    };
+    //- the hook function update results in re render of whole component
     return (
-        <div className="toggler">
-            <p onClick={update}>{isHappy ? "😄" : "😭"}</p>
+        <div>
+            <p className="toggler" onClick={update}>
+                {isHappy ? "😄" : "😭"}
+            </p>
+            <p>{count}</p>
         </div>
     );
 }
