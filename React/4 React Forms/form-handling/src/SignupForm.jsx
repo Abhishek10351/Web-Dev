77f0617980e39ccddce7392cb9a61803754ab4b5
Abhishek Kashyap <abhik5567@gmail.com>
2024-05-28 15:40:36 +0530

    add signup form code

--- React/4 React Forms/form-handling/src/SignupForm.jsx
+++ React/4 React Forms/form-handling/src/SignupForm.jsx
@@ -1,5 +1,34 @@
 import { useState } from "react";
 
 export default function SignupForm() {
-    
+    const [firstName, setFirstName] = useState("");
+    const [lastName, setLastName] = useState("");
+    const updateFirstName = (evt) => {
+        setFirstName(evt.target.value);
+    };
+    const updateLastName = (evt) => {
+        setLastName(evt.target.value);
+    };
+
+    return (
+        <>
+            <input
+                type="text"
+                value={firstName}
+                onInput={updateFirstName}
+                id="firstName"
+            />
+            <label htmlFor="#firstName">First Name</label>
+            <br />
+            <input
+                type="text"
+                value={lastName}
+                onInput={updateLastName}
+                id="lastName"
+            />
+            <label htmlFor="#lastName">Last Name</label>
+            <br />
+            <button>Sign Up</button>
+        </>
+    );
 }
