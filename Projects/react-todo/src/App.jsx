0b8519b4b6fb3687c0b58257153b9a342bc13789
Abhishek Kashyap <abhik5567@gmail.com>
2024-06-02 18:49:53 +0530

    add basic todo code

---
+++ Projects/react-todo/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import Icon from "@mui/material/Icon";
+import DeleteIcon from "@mui/icons-material/Delete";
+import CssBaseline from "@mui/material/CssBaseline";
+function App() {
+    return (
+        <>
+            <CssBaseline />
+        </>
+    );
+}
+
+export default App;
